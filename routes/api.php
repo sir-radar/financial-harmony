8e5a1106d87e8fa907f12ea347557360e22d424b
Samson Nwokike <nwokikesamson@gmail.com>
2025-09-14 13:59:00 +0100

    added api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/accounts', [App\Http\Controllers\AccountController::class, 'create']);
+Route::get('/accounts/number/{number}', [App\Http\Controllers\AccountController::class, 'findByNumber']);
+Route::get('/accounts/ssn/{ssn}', [App\Http\Controllers\AccountController::class, 'findBySsn']);
+Route::get('/accounts/balance/{min}/{max}', [App\Http\Controllers\AccountController::class, 'findByBalanceRange']);
+
+Route::post('/transactions', [App\Http\Controllers\TransactionController::class, 'create']);
+Route::get('/transactions/account/{accountNumber}', [App\Http\Controllers\TransactionController::class, 'findByAccountNumber']);
+Route::get('/transaction/amount/{min}/{max}', [App\Http\Controllers\TransactionController::class, 'findByAmountRange']);
